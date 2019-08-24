58f0ce386b816cc1d5ff45818de959c5f28280e8
Matthew Randall <mattrandall01@gmail.com>
2019-08-24 16:35:47 -0400

    set up arrow icon for create menu option

--- task_manager/lib/ui/widgets/general/mainmenu/menu_drawer.dart
+++ task_manager/lib/ui/widgets/general/mainmenu/menu_drawer.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:task_manager/ui/widgets/general/mainmenu/menu_item.dart';
 import 'package:task_manager/ui/ui_colors.dart';
 
-class MenuDrawer extends StatelessWidget{
+class MenuDrawer extends StatefulWidget{
+  @override
+  State createState() => MenuDrawerState();
+}
 
-  final List<String> list = ["Task", "Group", "Rank"];
+class MenuDrawerState extends State<MenuDrawer>{
+
+  List<String> list = ["Task", "Group", "Rank"];
+  IconData iData = Icons.keyboard_arrow_down;
 
   @override
   Widget build(BuildContext context){
@@ -14,16 +20,22 @@ class MenuDrawer extends StatelessWidget{
         child: ListView(
           padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
           children:[
-            Container(
+            Container( //TODO: Move to its own widget class
               color: Theme.of(context).accentColor,
               child: ExpansionTile(
+                onExpansionChanged: (context){ 
+                  setState(() {
+                    context ? iData = Icons.keyboard_arrow_up : iData = Icons.keyboard_arrow_down;
+                  });
+                },
+                trailing: Icon(iData, color: UIColors.offBlue,),
                 title: Text(
-                  "Create",
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: UIColors.offBlue,
-                  )
-                ),
+                    "Create",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: UIColors.offBlue,
+                    ),
+                  ),
                 children:[
                   MenuItem(value: "Task", icon: Icons.add_circle_outline, backgroundColor: UIColors.subMenuBlue ),
                   MenuItem(value: "Group", icon: Icons.add_circle_outline, backgroundColor: UIColors.subMenuBlue ),
@@ -32,7 +44,7 @@ class MenuDrawer extends StatelessWidget{
                 ],
               ),
             ),
-            Container(
+            Container( //TODO: Move to its own widget class
               color: Theme.of(context).accentColor,
               child: ListTile(
                 title: Row(
