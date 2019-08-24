06eb449c497df0ecddd8429914dbb472c1c26ef0
Matthew Randall <mattrandall01@gmail.com>
2019-08-23 21:23:00 -0400

    Added expandable list and placeholder icons

--- task_manager/lib/ui/widgets/general/mainmenu/menu_drawer.dart
+++ task_manager/lib/ui/widgets/general/mainmenu/menu_drawer.dart
@@ -1,17 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/ui/widgets/general/mainmenu/menu_item.dart';
+import 'package:task_manager/ui/ui_colors.dart';
 
 class MenuDrawer extends StatelessWidget{
+
+  final List<String> list = ["Task", "Group", "Rank"];
+
   @override
   Widget build(BuildContext context){
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
-        children:[
-          MenuItem(value: "one", icon: Icons.keyboard_arrow_down, backgroundColor: Theme.of(context).accentColor,),
-          MenuItem(value: "two", icon: Icons.keyboard_arrow_down, backgroundColor: Colors.amber),
-          MenuItem(value: "three", icon: Icons.keyboard_arrow_down,backgroundColor: Theme.of(context).accentColor ),
-        ],
+      child: Container( 
+        color: Theme.of(context).primaryColor,
+        child: ListView(
+          padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+          children:[
+            Container(
+              color: Theme.of(context).accentColor,
+              child: ExpansionTile(
+                title: Text(
+                  "Create",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: UIColors.offBlue,
+                  )
+                ),
+                children:[
+                  MenuItem(value: "Task", icon: Icons.add_circle_outline, backgroundColor: UIColors.subMenuBlue ),
+                  MenuItem(value: "Group", icon: Icons.add_circle_outline, backgroundColor: UIColors.subMenuBlue ),
+                  MenuItem(value: "Filter", icon: Icons.add_circle_outline, backgroundColor: UIColors.subMenuBlue ),
+                  MenuItem(value: "Priority", icon: Icons.add_circle_outline, backgroundColor: UIColors.subMenuBlue ),           
+                ],
+              ),
+            ),
+            Container(
+              color: Theme.of(context).accentColor,
+              child: ListTile(
+                title: Row(
+                  children:[
+                    Text(
+                      "Settings",
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: UIColors.offBlue,
+                      ),
+                    ),
+                    Spacer(),
+                    Icon(
+                      Icons.settings,
+                      color: UIColors.offBlue,
+                    ),
+                  ]
+                ),
+              )
+            ),
+          ],
+        ),
       ),
     );
   }
